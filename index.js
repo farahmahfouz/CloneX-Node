11edf4dcf6baca96e62104355574c7b5a4836fd0
farahmahfouz <farahmahfouz11@gmail.com>
2025-02-07 17:58:13 +0200

    Feat/ add refresh token and integrate with Google

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const cors = require("cors");
-const morgan = require('morgan');
+const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const session = require("express-session");
+require("./utils/passport");
 require("express-async-errors");
-
 
 const userRouter = require("./Routes/usersRoutes");
 const postRouter = require("./Routes/postsRoutes");
+const authRouter = require("./Routes/authRoutes");
 
 const logger = require("./utils/logger");
 const AppError = require("./utils/App.Error");
@@ -16,12 +19,20 @@ const AppError = require("./utils/App.Error");
 const PORT = 8000;
 const app = express();
 
-dotenv.config();
-
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
+app.use(
+  session({
+    secret: "your_secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.send("Hello From Another World");
@@ -29,6 +40,7 @@ app.get("/", (req, res) => {
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
+app.use("/api/auth", authRouter);
 
 app.all("/*", (req, res, next) => {
   throw new AppError(
---
+++ utils/passport.js
@@ -0,0 +1,46 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../Models/usersModel");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_BACK_URL,
+      scope: ["profile", "email"],
+    },
+
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+    
+        if (!user) {
+          user = await User.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            avatar: profile.photos[0].value,
+            password: Math.random().toString(36).slice(-8) 
+          });
+        }
+        done(null, user);
+      } catch (err) {
+        done(err, null);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
