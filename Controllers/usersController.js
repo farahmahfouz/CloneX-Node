954b500397883a09dda189d2937c0a9d1f443bf0
farahmahfouz <farahmahfouz11@gmail.com>
2025-05-13 16:57:00 +0300

    FIX/ allow login from Vercel by setting correct CORS and cookie options

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -150,7 +150,7 @@ exports.login = async (req, res, next) => {
 
     res.cookie('jwt', refreshToken, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost:5173' || 'https://clone-x-khaki.vercel.app/', 
     credentials: true,
   })
 );
