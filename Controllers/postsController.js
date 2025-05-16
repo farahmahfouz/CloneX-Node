85a17390e46047712a80e1f20f9798ea598df2c8
farahmahfouz <farahmahfouz11@gmail.com>
2025-05-16 16:02:56 +0300

    Sorting posts by date

--- Controllers/postsController.js
+++ Controllers/postsController.js
@@ -6,7 +6,7 @@ const isPostOwner = (postUserId, currentUserId) =>
   postUserId.toString() === currentUserId.toString();
 
 exports.getAllPosts = catchAsync(async (req, res) => {
-  const posts = await Post.find();
+  const posts = await Post.find().sort({ createdAt: -1 });
   if (!posts) {
     throw new AppError('No Posts Found', 404);
   }
